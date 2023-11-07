e1d292c4f56523c89160ff9bb094710019412282
Vizeu <leovizeu@hotmail.com>
2023-11-07 19:23:59 -0300

    changed the code to satisfy new version of Selenium, if it still doesn't work after a couple tests, i'll either change back or use a solution that might work in the TypeError Forum

--- whatsbot.py
+++ whatsbot.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -7,19 +9,21 @@ class Whatsbot:
         # Parte 1 - A mensagem que você quer enviar
         self.mensagem = "Bom dia, Seja Bem vindo à loja Vizeu Design MDF"
         # Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
-        self.grupos_ou_pessoas = ["grupo de teste", "grupo de vendas"]
-        options = webdriver.ChromeOptions()
-        options.add_argument('lang=pt-br')
-        self.driver = webdriver.Chrome(
-            executable_path=r'./chromedriver.exe', chrome_options=options)
-            
+        self.grupos_ou_pessoas = ["grupo de teste"]
+        service = Service(executable_path='./chromedriver.exe')
+        option = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(service=service, options = option)
+        option.add_argument('lang=pt-br')
+        self.driver.quit()
+        
 
     def enviar_mensagem(self):
         #<span dir="auto" title="grupo de teste" aria-label="" class="ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr _11JPr" style="min-height: 0px;">grupo de teste</span>
         #<div class="_1VZX7">
         #<span data-icon="send" class="">
         #<span dir="auto" title="grupo de vendas" aria-label="" class="ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr _11JPr" style="min-height: 0px;">grupo de vendas</span>
-        self.driver.get('https://web.whatsapp.com')
+        driver=webdriver.Chrome()
+        driver.get('https://web.whatsapp.com')
         time.sleep(30)
         for grupo_ou_pessoa in self.grupos_ou_pessoas:
             campo_grupo = self.driver.find_element(By.XPATH, f'//span[@title="{grupo_ou_pessoa}"]')
@@ -33,7 +37,10 @@ class Whatsbot:
             time.sleep(3)
             botao_enviar.click()
             time.sleep(5)
+        
 
 
-bot = Whatsbot()
+if __name__ == "__main__":
+    bot = Whatsbot()
+    
 bot.enviar_mensagem()
